3b7a508c6da0ffd4410e481491f26123e0aed00a
849344 <64076272+849344@users.noreply.github.com>
2020-05-04 13:00:00 +0530

    Create DateUtil.java

---
+++ src/com/cognizant/truyum/util/DateUtil.java
@@ -0,0 +1,13 @@
+package com.cognizant.truyum.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+	public static Date convertToDate(String date) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		return format.parse(date);
+	}
+
+}
